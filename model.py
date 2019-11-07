94d0f542ba0301f7ec8782c25f717aa2b7adf187
Brian Barbieri <31993935+brianbarbieri@users.noreply.github.com>
2019-11-07 11:02:30 +0100

    Updated model to be able to handle train and test data

--- model.py
+++ model.py
@@ -4,10 +4,14 @@ import pandas as pd
 import random
 from itertools import permutations 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 class Individual:
-    def __init__(self, w1=None, w2=None, b1=None, b2=None):
+    def __init__(self, mutation_rate=0.1, w1=None, w2=None, b1=None, b2=None):
+        self.mutation_rate = mutation_rate
         if w1 is None:
             self.w1 = np.random.randn(4,3)
         else:
@@ -27,6 +31,9 @@ class Individual:
             
     def network(self, x):
         
+        def sigmoid(z):
+            return 1/(1+np.exp(-z))
+        
         def relu(z):
             return np.maximum(0, z)
 
@@ -39,13 +46,17 @@ class Individual:
         return yhat
     
     def calc_cost(self, x, y):
-        y_hat =  np.round(self.network(x)) ##round to 0 or 1 to get class
-        return f1_score(y, y_hat.flatten(), average="macro")
+        y_hat = self.predict(x) 
+        return f1_score(y, y_hat, average="macro")
     
+    def predict(self, x):
+        return np.round(self.network(x)).flatten() ##round to 0 or 1 to get class
+        
     def reproduce(self, other_ind, children=2):
         
         def mutate(char):
-            tomutate = np.random.choice([True, False], char.shape, p =[0.1, 0.9])
+            mut=[self.mutation_rate, 1-self.mutation_rate]
+            tomutate = np.random.choice([True, False], char.shape, p =mut)
             mutations =  np.random.randn(*char.shape)
             char[tomutate] = mutations[tomutate]
             return char
@@ -56,44 +67,62 @@ class Individual:
             child_w1 = mutate(child_w1)
             w2_prob = np.random.randint(0,2, other_ind.w2.shape)
             child_w2 = np.select([w2_prob==0, w2_prob==1],[self.w2, other_ind.w2])
+            child_w2 = mutate(child_w2)
             b1_prob = np.random.randint(0,2, other_ind.b1.shape)
             child_b1 = np.select([b1_prob==0, b1_prob==1],[self.b1, other_ind.b1])
+            child_b1 = mutate(child_b1)
             b2_prob = np.random.randint(0,2, other_ind.b2.shape)
             child_b2 = np.select([b2_prob==0, b2_prob==1],[self.b2, other_ind.b2])
-            ind = Individual(child_w1, child_w2, child_b1, child_b2)
+            child_b2 = mutate(child_b2)
+            ind = Individual(self.mutation_rate, child_w1, child_w2, child_b1, child_b2)
             return ind
             
         kids = [get_child() for _ in range(children)]
         return kids
-
 def get_top(pop, x, y, best=5):
     cost_of_pop = [1-ind.calc_cost(x, y) for ind in pop]
     return np.take(pop, np.argsort(cost_of_pop)[:best])
 
-def new_generation(best_indus):
-    new_pop = []
+def new_generation(best_indus, add_parents):
+    if add_parents:
+        new_pop = [*best_indus]
+    else:
+        new_pop = []
     permus = list(permutations(best_indus, 2))
     for indu1, indu2 in permus:
-        new_pop += indu1.reproduce(indu2, 5)
+        new_pop += indu1.reproduce(indu2, children=5)
     return new_pop
 
-
-def main(x, y, generations=5, best=5, start_population=5):
-    population = [Individual() for _ in range(start_population)]
+def main(X_train, y_train, X_test, y_test, generations=5, best=5, start_population=5, mutation_rate=0.1, add_parents=True):
+    population = [Individual(mutation_rate) for _ in range(start_population)]
     for i in range(generations):
-        best_induviduals = get_top(population, x, y, best=5)
-        pop_mean = np.mean([bindu.calc_cost(x, y) for bindu in best_induviduals])
-        print(f"Gen {i+1}, fitness F1-score is {pop_mean}")
-        population = new_generation(best_induviduals) 
-    score = best_induviduals[0].calc_cost(x,y)
-    print(f"Finished generic optimization algorithm, best obtained model achieved a training F1-score of {score}")
+        best_induviduals = get_top(population, X_train, y_train, best=5)
+        pop_mean_train = np.mean([bindu.calc_cost(X_train, y_train) for bindu in best_induviduals])
+        pop_mean_test = np.mean([bindu.calc_cost(X_test, y_test) for bindu in best_induviduals])
+        print(f"Gen {i+1}, fitness F1-score is {pop_mean_train} and a test F1-score of {pop_mean_test}")
+        population = new_generation(best_induviduals, add_parents) 
+    score_train = best_induviduals[0].calc_cost(X_train,y_train)
+    score_test = best_induviduals[0].calc_cost(X_test,y_test)
+    print(f"Finished generic optimization algorithm")
+    print(f"Best obtained model achieved a training F1-score of {score_train}")
+    print(f"Best obtained model achieved a test F1-score of {score_test}")
     
 
 if __name__ == "__main__":
     #read in the data
     df = pd.read_csv("neural_generic_algo_test_data.csv")
-    x = df.drop("y", axis=1).values
-    x = np.rot90(x)
-    y = df["y"].values
+    df_shuffles = shuffle(df, random_state=42) # always shuffle your data to avoid any biases that may emerge b/c of some order
+    train, test = train_test_split(df_shuffles, test_size=0.3, random_state=42)
 
-    main(x, y, generations=20)
+    x_train = train.drop("y", axis=1).values
+    y_train = train["y"].values
+    X_train = np.rot90(x_train)
+
+    x_test = test.drop("y", axis=1).values
+    y_test = test["y"].values
+    X_test = np.rot90(x_test)
+
+    main(X_train, y_train, X_test, y_test,
+         generations=50, best=5,
+         add_parents=True, mutation_rate=0.1)
+
